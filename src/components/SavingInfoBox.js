18f9bfc2500404d48fa57f2bba6bc583f269c8c4
siyabongahenry <siyabongahenry23@gmail.com>
2023-09-17 18:56:19 +0200

    updates

--- src/components/SavingInfoBox.js
+++ src/components/SavingInfoBox.js
@@ -1,24 +1,39 @@
+import { useState } from "react";
 import { FaFilePdf, FaInfoCircle, FaLaptop, FaMobile } from "react-icons/fa";
 const SavingInfoBox = ()=>{
+    const[infoToggle,setInfoToggle] = useState(false);
     return(
-        <section className="alert alert-info d-inline-block">
-            <p>
-                <FaInfoCircle/> 
-            </p>
-            <p className="text-start">
-                &nbsp;
-                <h3>HOW TO SAVE YOUR CV IN YOUR MOBILE DEVICE <FaMobile/></h3>
-                <ol>
-                    <li>Click the "SAVE MY CV" button below</li>
-                    <li>Look for "PAPER SIZE" and select A4</li>
-                    <li>Now click the yellow pdf download button</li>
-                    <li>Choose a folder in your device and press save</li>
-
-                </ol>
-            </p>
-            <p>
-                <FaLaptop/>&nbsp;If you're using a Desktop select "Print To PDF", then "Print" to save your document Locally.
-            </p>
+        <section  onClick={()=>setInfoToggle(!infoToggle)} style={{cursor:"pointer"}} className="alert alert-info d-inline-block text-start">
+            <h2><FaInfoCircle/> HOW TO SAVE YOUR DOCUMENT AS A PDF??</h2>
+            {
+                infoToggle &&
+                <>
+                    <section>
+                        &nbsp;
+                        <h4>MOBILE DEVICE <FaMobile/></h4>
+                        <ol>
+                            <li>Click the "SAVE ..." button below</li>
+                            <li>Look for "PAPER SIZE" and select A4</li>
+                            <li>You can thereafter click the yellow pdf download button</li>
+                            <li>Choose a folder in your device and press save</li>
+                        </ol>
+                    </section>
+                    <section>
+                        <h4>DESKTOP<FaLaptop/></h4>
+                        <ol>
+                            <li>Click the "SAVE MY DOCUMENT" button below</li>
+                            <li>Change your printer to "PRINT TO PDF"</li>
+                            <li>Look for "MORE SETTINGS"</li>
+                            <li>Set your "PAPER SIZE" to "A4"</li>
+                            <li>Click "PRINT", select a folder and save</li>
+                        </ol>
+        
+                    </section>
+                    <p className="text-danger text-center">
+                        CLICK ANYWHERE TO HIDE THE ABOVE INFORMATION
+                    </p>
+                </>
+            }
         </section>
     )
 }
